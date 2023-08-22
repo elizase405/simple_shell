dd54d6d67924227e84a512a75cdc26cedfe755dd
eliza <elizase405@gmail.com>
2023-08-22 07:42:53 -0700

    made shell an infinite loop

--- shell.c
+++ shell.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 /**
  * main - Start of a unix command line interpreter
@@ -21,46 +23,59 @@ int main(int argc, char **argv, char **env)
 		char *token;
 		char *delim = " ";
 		int i = 0, j;
+		pid_t cid;
 		size_t n = 0;
-		
+
 		buf = NULL;
-		
-		write(1, "$ ", 2);
-		
+
+		write(STDIN_FILENO, "$ ", 2);
+
 		if (getline(&buf, &n, stdin) == -1)
 		{
+			free(buf);
 			perror("getline failed");
 			return (-1);
 		}
-		
 		token = strtok(buf, delim);
 		_argv = malloc(sizeof(char *) * n);
 
-		
 		while (token)
 		{
 			_argv[i] = token;
 			token = strtok(NULL, delim);
 			i++;
 		}
-		
+
 		argv[i] = NULL;
-		
+
 		for (i = 0; _argv[i]; i++)
 		{
 			for (j = 0; _argv[i][j]; j++)
 				;
 		}
 
-		_argv[i-1][j-1] = '\0';
+		_argv[i - 1][j - 1] = '\0';
+		cid = fork();
 
-		if (execve(_argv[0], _argv, NULL) == -1)
+		if (cid == -1)
 		{
-			perror("execve failed");
+			perror("cid failed");
 			return (-1);
 		}
 
-		free(buf), free(_argv);
+		if (cid == 0)
+		{
+			if (execve(_argv[0], _argv, NULL) == -1)
+			{
+				perror("execve failed");
+				return (-1);
+			}
+		}
+		else
+		{
+			wait(NULL);
+			free(buf), free(_argv);
+		}
 	}
 	return (0);
 }
